04c9ad1130e5b1c80d169166f0cce072b807482f
abaran23 <99424908+abaran23@users.noreply.github.com>
2023-09-26 16:08:44 +0300

    Update Username.jsx

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 function Username() {
   const username = useSelector((state) => state.user.username);
   return (
-    <div className="hidden text-sm font-semibold md:block">{username}</div>
+    <div className="hidden text-sm font-semibold md:block text-slate-100">{username}</div>
   );
 }
 
